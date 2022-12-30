cbcfa2023a58afea6f9d0a977dc279157a4a6d99
Gorbunov Vladislav <wint-mil@mail.ru>
2022-12-30 16:23:50 +0300

    Added concerts, audios, users.

---
+++ src/main/java/com/cvetkov/fedor/laboratoryworkmicro/subscriptions/SubscriptionsApplication.java
@@ -0,0 +1,15 @@
+package com.cvetkov.fedor.laboratoryworkmicro.subscriptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SubscriptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cvetkov/fedor/laboratoryworkmicro/subscriptions/SubscriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvetkov.fedor.laboratoryworkmicro.subscriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
